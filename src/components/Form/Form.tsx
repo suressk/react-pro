aa1046c5326f9e6af6ef968332eb40377db24e58
suressk <sure_k@qq.com>
2021-07-17 13:22:00 +0800

    fix(src/components/Form/*): fixed the type of Form Component

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { Provider } from './context'
+
+interface IFormProps {
+    onSubmit: (data: object) => void
+}
+
+export default class Form extends Component<IFormProps> {
+    state = {
+        formData: {},
+        changeFormData: (name: string, val: any) => {
+            this.setState({
+                formData: {
+                    ...this.state.formData,
+                    [name]: val
+                }
+            })
+        },
+        submit: () => {
+            this.props.onSubmit && this.props.onSubmit(this.state.formData)
+        }
+    }
+    render() {
+        return (
+            <Provider value={this.state}>
+                {this.props.children}
+            </Provider>
+        )
+    }
+}
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,42 +1,18 @@
-import React, { Component } from 'react'
-import { Provider } from './context'
+import InternalForm from './Form'
 import Input from './Input'
 import Button from './Button'
 
-interface IFormProps {
-    onSubmit: (data: object) => void
+type InternalFormType = typeof InternalForm;
+
+interface FormInterface extends InternalFormType {
+    Input: typeof Input
+    Button: typeof Button
 }
 
-// interface IFromSS {
-//   Input: Component
-//   Button: Component
-// }
+const Form = InternalForm as FormInterface
 
-export default class Form extends Component<IFormProps, {}, any> {
-    state = {
-        formData: {},
-        changeFormData: (name: string, val: any) => {
-            this.setState({
-                formData: {
-                    ...this.state.formData,
-                    [name]: val
-                }
-            })
-        },
-        submit: () => {
-            this.props.onSubmit && this.props.onSubmit(this.state.formData)
-        }
-    }
-    render() {
-        return (
-            <Provider value={this.state}>
-                {this.props.children}
-            </Provider>
-        )
-    }
-}
-
-/* @ts-ignore */
 Form.Input = Input
-/* @ts-ignore */
+
 Form.Button = Button
+
+export default Form
